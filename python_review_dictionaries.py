0d574e78e099de66604f4d4bfa441e21b70cdc28
Jemyishai <jesse.moskowitz@gmail.com>
2020-04-04 14:33:39 -0400

    wrote out functions and accounted for multiple cities

--- python_review_dictionaries.py
+++ python_review_dictionaries.py
@@ -18,14 +18,49 @@ Shanghai (China, Asia)"""
 locations = {'North America': {'USA': ['Mountain View']}}
 
 def cities_to_add (city,country,continent):
-    if locations[continent]:
-       print 'made it continent'
-       if len(locations[continent][country])>0:
-           print 'made it country'
-           if locations[continent][country].index(city)>-1:
-               print 'made it city'
+    continent_key = locations.get(continent, 'NOPE')
+    if continent_key != 'NOPE':
+      country_key = continent_key.get(country,"NOPE")
+      if country_key != "NOPE":
+          #this check is needed in the event there is more than one city in the country
+          if city not in country_key:
+              country_key.append(city)
+              country_key.sort()
+          else:
+              print 'city already added'
+      else:
+          continent_key[country] = [city]
+    else:
+        locations[continent] = {country: [city]}
 
-print cities_to_add("Mountain View","USA","North America")
+cities_to_add("Shanghai","China","Asia")
+cities_to_add("Bangalore","India","Asia")
+cities_to_add("Atlanta","USA","North America")
+cities_to_add("Cairo","Egypt","Africa")
+
+# print(locations)
+
+def print_array(input):
+    for item in input:
+        print item
+
+def asia_alpha_order(location):
+    asiancities = []
+    citiescountry = []
+    asian_co = location['Asia']
+    for key in asian_co:
+        asiancities = asiancities + asian_co[key]
+        asiancities.sort()
+    for city in asiancities:
+        for country in asian_co.keys():
+            if city in asian_co[country]:
+                citiescountry.append(city + ' - ' + country)
+    return citiescountry
+
+print('1')
+print_array(locations['North America']['USA'])
+print('2')
+print_array(asia_alpha_order(locations))
 
 """Print the following (using "print").
 1. A list of all cities in the USA in
