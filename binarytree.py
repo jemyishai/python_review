097253818a0a24876b92668188b9bc01906f882e
Jemyishai <jesse.moskowitz@gmail.com>
2020-04-07 14:37:59 -0400

    working on binary tree algorithms

---
+++ binarytree.py
@@ -0,0 +1,13 @@
+  Binary Tree Practice
+            1
+          /  \
+         2     3
+        /       \
+       4         5
+
+##oneof the larger issues is that I'm not familiar with the self
+
+def preorder_print():
+  if self.left == None and self.right == None:
+    traversal.append(value)
+
